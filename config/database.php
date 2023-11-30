11a418da54d119d86e63c1d5112b771f0db949ae
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2023-11-30 16:13:03 -0300

    Adicionando funções de Redis

--- config/database.php
+++ config/database.php
@@ -128,6 +128,8 @@ return [
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
+        'default' => ['url' => env ('REDIS_URL')],
+/*
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -136,6 +138,7 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
+*/
 
         'cache' => [
             'url' => env('REDIS_URL'),
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Redis;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,6 +24,19 @@ Route::get ('/info', function () {
 });
 
 Route::view ("/ver", "ver");
+
+Route::get ("/redis/keys", function () {
+    return Redis::command ("keys", ["*"]);
+});
+
+Route::get ("/redis/get/{key}", function (string $key) {
+    return "Valor da chave $key: " . Redis::get ($key);
+});
+
+Route::get ("/redis/set/{key}/{value}", function (string $key, string $value) {
+    Redis::set ($key, $value);
+    return "Definida chave $key com valor $value";
+});
 
 Route::fallback (function () {
     return "Função ainda não implementada";
